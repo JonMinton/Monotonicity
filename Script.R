a70a6a2610cab6f11bf30e4044e4656498084e15
Jonathan Minton <nate.minton@gmail.com>
2014-11-26 08:22:22 +0000

    re-run figs with additional observation

--- Script.R
+++ Script.R
@@ -63,30 +63,3 @@ source("scripts/Make_Figures.R")
 
 
 
-# To do:
-
-# Add dynamic notebooks which record critical parameters, summaries etc.
-# Add subsection showing dependence of AIVM and cov methods on number of draws
-# produce tables and caterpillar plots showing distribution of parameters
-
-
-
-# Figures
-# Redraw all figures in ggplot2
-# First part (all ten methods)
-# Label axes as 'better health state' & 'worse health state
-#     Second part (subset of methods)
-#         label axes as 'low health state', 'moderate health state', 'high health state'
-#     Include names of methods in figures
-#     Look at density plot equivalents of violin plots
-#     ACTION: include multiple measures of central tendency in violin plots/density plots
-# Further analyses
-#     IPD
-#         Additional two state IPD
-#             Example where not all individual differences were positive
-#         Additional three state IPD
-#     Aggregate
-#         Two state Different N
-#         Three state different N
-
-
